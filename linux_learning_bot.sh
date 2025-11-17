7fc26daea0a46c394300a554896c0e710624a037
Md. Fahim Hossain <fahimhossain5485@gmail.com>
2025-11-18 03:23:16 +0600

    Update linux_learning_bot.sh

--- linux_learning_bot.sh
+++ linux_learning_bot.sh
@@ -21,7 +21,7 @@ greet_user() {
     echo -n "Enter your name: "
     read name
     echo "Greetings, $name!"
-   
+    
     echo "Press Enter to start your Linux Journey"
     read
     main_menu
@@ -44,27 +44,67 @@ teach_basic_commands() {
     echo "Command Explanations"
     echo "-------------------------------------------"
     echo
+
     echo "pwd (Print Working Directory)"
     echo " > Purpose: Shows your current directory location."
     echo " > Syntax: pwd"
     echo " > Example: pwd → /home/user/Documents"
     echo
+
     echo "cd (Change Directory)"
     echo " > Purpose: Moves you into another directory."
     echo " > Syntax: cd <directory_name>"
     echo " > Example: cd /home/user/Desktop"
     echo
+
     echo "ls (List)"
     echo " > Purpose: Lists files and folders in the current directory."
     echo " > Syntax: ls [options]"
     echo " > Example: ls -l → lists details like permissions, owner, size."
     echo
-    echo "-------------------------------------------"
-    echo "Press ENTER to start a short quiz..."
-    read
-    quiz_basic_commands
+
+   
+
+    echo "cat (Concatenate and View Files)"
+    echo " > Purpose: Displays the content of files on the terminal."
+    echo " > Syntax: cat <filename>"
+    echo " > Example: cat notes.txt → shows the content of notes.txt"
+    echo
+
+    echo "man (Manual Pages)"
+    echo " > Purpose: Opens the manual/help page for any command."
+    echo " > Syntax: man <command>"
+    echo " > Example: man ls → shows documentation for ls"
+    echo
+
+    echo "help (Built-in Shell Help)"
+    echo " > Purpose: Shows help information for shell built-in commands."
+    echo " > Syntax: help <command>"
+    echo " > Example: help cd → shows help info about cd"
+    echo
+
+  
     
+    echo "-------------------------------------------"
+    echo "Press ENTER to start a short quiz or type 'menu' to return to main menu"
+
+    while true; do
+        read cmd
+        
+        if [[ -z $cmd ]]; then
+            quiz_basic_commands
+            break
+
+        elif [[ $cmd == "menu" ]]; then
+            main_menu
+            break
+
+        else
+            echo "Invalid input. Please press ENTER or type 'menu'."
+        fi
+    done
 }
+
 # ---------- FUNCTION: Teaching File Operation ----------
 teach_file_operations() {
     clear
@@ -97,81 +137,128 @@ teach_file_operations() {
     echo " Be careful! Deleted files usually cannot be recovered."
     echo
     echo "-------------------------------------------"
-    echo "Press ENTER to start a short quiz..."
-    read
-    quiz_file_operations
+      echo "Press ENTER to start a short quiz or type 'menu' to return to main menu"
+
+while true; do
+    read cmd
+    
+    if [[ -z $cmd ]]; then
+        
+        quiz_file_operations
+        break
+
+    elif [[ $cmd == "menu" ]]; then
+        main_menu
+        break
+
+    else
+        echo "Invalid input. Please press ENTER or type 'menu'."
+    fi
+done
    
 }
 
 quiz_file_operations() {
     clear
     echo "========== FILE OPERATIONS QUIZ =========="
+    echo "There are total five question. Type the correct answer or"
+    echo "Type 'menu' to return to main menu or 'back' to return back to previous menu"
     echo
     score=0
 
     # Question 1
     echo "1. How do you create a file called 'myfile'?"
-    
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "touch myfile" ]]; then
+    if [[ $ans == "touch myfile" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == 'menu' ]]; then
+        main_menu
+        return
+    elif [[ $ans == 'back' ]]; then
+        teach_file_operations
+        return
     else
         echo "Wrong! The correct answer is: touch myfile"
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 2
     echo "Which command creates a new directory named 'docs'?"
-   
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "mkdir docs" ]]; then
+    if [[ $ans == "mkdir docs" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == 'menu' ]]; then
+        main_menu
+        return
+    elif [[ $ans == 'back' ]]; then
+        teach_file_operations
+        return
     else
         echo "Wrong! The correct answer is: mkdir docs"
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 3
     echo "How do you copy a file named 'report.txt' to 'backup.txt'?"
-  
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "cp report.txt backup.txt" ]]; then
+    if [[ $ans == "cp report.txt backup.txt" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == 'menu' ]]; then
+        main_menu
+        return
+    elif [[ $ans == 'back' ]]; then
+        teach_file_operations
+        return
     else
         echo "Wrong! The correct answer is: cp report.txt backup.txt"
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 4
     echo "Using the \"mv\" command, how would you rename a file named \"cat\" to \"dog\"?"
-   
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "mv cat dog" ]]; then
+    if [[ $ans == "mv cat dog" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == 'menu' ]]; then
+        main_menu
+        return
+    elif [[ $ans == 'back' ]]; then
+        teach_file_operations
+        return
     else
         echo "Wrong! The correct answer is: mv cat dog"
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 5
     echo "How do you delete a file named 'old.txt'?"
- 
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "rm old.txt" ]]; then
+    if [[ $ans == "rm old.txt" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == 'menu' ]]; then
+        main_menu
+        return
+    elif [[ $ans == 'back' ]]; then
+        teach_file_operations
+        return
     else
         echo "Wrong! The correct answer is: rm old.txt"
     fi
+    echo "Current Score: $score"
     echo
 
     echo "-------------------------------------------"
@@ -180,85 +267,167 @@ quiz_file_operations() {
 
     if [ $score -eq 5 ]; then
         echo "Excellent! You’ve mastered file operations!"
-        
         echo "Press Enter to Return to main menu..."
         read
-        
         main_menu
     else
         echo "You got some answers wrong. Want to try again?"
-        echo "Type 'retry' to retake the quiz or 'menu' to go back to the main menu."
-        read choice
-        if [ "$choice" = "retry" ]; then
-            quiz_file_operations
-        else
-            main_menu
-        fi
+        echo "Type 'retry' to retake the quiz or 'menu' to go back to the main menu or 'back to previous menu'"
+
+        while true; do
+            read choice
+            if [[ $choice == "retry" ]]; then
+                quiz_file_operations
+                break
+            elif [[ $choice == "menu" ]]; then
+                main_menu
+                break
+                elif [[ $choice == "back" ]]; then
+                teach_file_operations
+                break
+            else
+                echo "Invalid input"
+            fi
+        done
     fi
 }
-
 
 quiz_basic_commands() {
     clear
     echo "========== BASIC COMMANDS QUIZ =========="
+    echo "There are total six questions. Enter correct answer or"
+    echo "Type 'menu' to return to main menu or 'back' to return back to previous menu"
     echo
     score=0
 
     # Question 1
     echo "1. Which command is used to find the directory you are currently in?"
-   
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "pwd" ]]; then
+    if [[ $ans == "pwd" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
     else
         echo "Wrong! The correct answer is 'pwd'."
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 2
     echo "2. Which command is used to change the current directory?"
-   
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "cd" ]]; then
+    if [[ $ans == "cd" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
     else
         echo "Wrong! The correct answer is 'cd'."
     fi
+    echo "Current Score: $score"
     echo
 
     # Question 3
     echo "3. Which command lists files in the current directory?"
-    
     echo -n "Your answer: "
     read ans
-    if [[ "$ans" == "ls" ]]; then
+    if [[ $ans == "ls" ]]; then
         echo "Correct!"
         ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
     else
         echo "Wrong! The correct answer is 'ls'."
     fi
+    echo "Current Score: $score"
+    echo
+
+    # Question 4 (cat)
+    echo "4. Which command is used to display the content of a file?"
+    echo -n "Your answer: "
+    read ans
+    if [[ $ans == "cat" ]]; then
+        echo "Correct!"
+        ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
+    else
+        echo "Wrong! The correct answer is 'cat'."
+    fi
+    echo "Current Score: $score"
+    echo
+
+    # Question 5 (man)
+    echo "5. Which command is used to view the manual page of another command?"
+    echo -n "Your answer: "
+    read ans
+    if [[ $ans == "man" ]]; then
+        echo "Correct!"
+        ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
+    else
+        echo "Wrong! The correct answer is 'man'."
+    fi
+    echo "Current Score: $score"
+    echo
+
+    # Question 6 (help)
+    echo "6. Which command shows help information about shell built-in commands?"
+    echo -n "Your answer: "
+    read ans
+    if [[ $ans == "help" ]]; then
+        echo "Correct!"
+        ((score++))
+    elif [[ $ans == "menu" ]]; then
+        main_menu
+        return
+    elif [[ $ans == "back" ]]; then
+        teach_basic_commands
+        return
+    else
+        echo "Wrong! The correct answer is 'help'."
+    fi
+    echo "Current Score: $score"
     echo
 
     echo "-------------------------------------------"
-    echo "Your total score: $score / 3"
+    echo "Your total score: $score / 6"
     echo "-------------------------------------------"
 
-    if [ $score -eq 3 ]; then
-        echo "Excellent! You’ve mastered the basics!"
-        
+    if [ $score -eq 6 ]; then
+        echo "Excellent! You've mastered the basics!"
         echo "Press Enter to return to main menu..."
         read
         main_menu
-        
     else
         echo "Keep practicing! Try reviewing the lesson again."
-        echo "Press ENTER to retry or type 'menu' to go back to main menu."
+        echo "Press any key to retry or type 'menu' to go back to main menu."
         read choice
-        if [ "$choice" = "menu" ]; then
+        if [[ $choice == "menu" ]]; then
             main_menu
         else
             quiz_basic_commands
@@ -289,7 +458,7 @@ main_menu() {
             4) learn_file_ops ;;
             5) learn_shell_scripting ;;
             6) echo "Goodbye, $name 👋"; exit 0 ;;
-            *) echo "Invalid choice! Try again.";;
+            *) echo "Invalid choice! Try again."; sleep 1 ;;
         esac
     done
 }
@@ -308,9 +477,9 @@ learn_basic_commands() {
     while true; do
         echo -n "> "
         read cmd
-        if [ "$cmd" = "back" ]; then
+        if [ $cmd = "back" ]; then
             return
-        elif [ "$cmd" = "q" ]; then
+        elif [ $cmd = "q" ]; then
             echo "Exiting program. Bye!"
             exit 0
         else
@@ -349,7 +518,7 @@ learn_file_ops() {
     while true; do
         echo -n "> "
         read cmd
-        if [ "$cmd" = "back" ]; then
+        if [ $cmd = "back" ]; then
             return
         else
             eval "$cmd"
@@ -360,29 +529,18 @@ learn_file_ops() {
 # ---------- learn shell scripting----------
 learn_shell_scripting() {
     clear
-    echo "========== SHELL SCRIPTING BASICS =========="
-    echo
-    echo "A shell script is a file with a .sh extension that contains commands"
-    echo "that the shell can execute line by line."
-    echo
+    echo "Shell Scripting Basics"
+    echo "----------------------------------"
+    echo "A shell script is a file with .sh extension that contains commands."
     echo "Example:"
     echo '    #!/bin/bash'
     echo '    echo "Hello World!"'
     echo
-    echo "You can make it executable using:"
-    echo "    chmod +x filename.sh"
-    echo
-    echo "Then run it with:"
-    echo "    ./filename.sh"
-    echo
-    echo "-------------------------------------------"
-    echo "Press ENTER to return to the main menu..."
+    echo "Press ENTER to return to menu..."
     read
-    main_menu
 }
 
 # ---------- MAIN PROGRAM ----------
 show_banner
 greet_user
 main_menu
-
